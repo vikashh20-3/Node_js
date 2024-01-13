b93123cb1aafe49535e9c31c36da0e7353b6d340
vikash <vvikashrao203@gmail.com>
2024-01-13 21:50:57 +0530

    read file code added

--- file.js
+++ file.js
@@ -3,3 +3,18 @@ const fs =require("fs");
 fs.writeFileSync('./test.txt',' Hello world ');
 //asynchronous 
 fs.writeFile("./testtwo.txt","hello viku",(err)=>{});
+
+
+// To read file
+const result=fs.readFileSync('./contact.txt','utf-8');
+console.log(result);
+
+// if you use asynchronous can't be return value in the varible 
+
+fs.readFile('./contact.txt','utf-8',(err,restult)=>{
+    if(err){
+        console.log("error",err)
+    }else{
+        console.log(result);
+    }
+});
