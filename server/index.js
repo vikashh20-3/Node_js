b095bb187f8148340d9a371ce2586f88f025f687
vikash <vvikashrao203@gmail.com>
2024-01-15 19:12:20 +0530

    url package added and adding queryparameter to our url

--- server/index.js
+++ server/index.js
@@ -1,19 +1,27 @@
 const http = require("http");
 const fs = require('fs');
+const url = require("url");
+
+
 const { error } = require("console");
 const myServer = http.createServer((request, response) => {
     if (request.url === '/favicon.ico') return response.end();
     // console.log('new req rec');
     // console.log(request.headers)
     const log = `${Date.now()}  ${request.url}: New Request Recieved\n`;
+    const myUrl = url.parse(request.url,true);
+    //here true make seperation of different query parameters
+    console.log(myUrl);
     fs.appendFile('log.txt', log, (error, data) => {
-        switch (request.url) {
+        // switch (request.url) {
+        switch (myUrl.pathname) {
 
             case "/":
                 response.end("homepage");
                 break;
             case "/about":
-                response.end("Vikash");
+                const username=myUrl.query.myname
+                response.end(`hi ${username}`);
                 break;
             default:
                 response.end("404 Page not found");
@@ -24,4 +32,4 @@ const myServer = http.createServer((request, response) => {
     // console.log(request);
 });
 
-myServer.listen(8000, () => console.log('Server Started'));
+myServer.listen(9000, () => console.log('Server Started'));
