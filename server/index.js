9bb15a0fae2d2c6a8b27e50ce531f2ad917e6f17
vikash <vvikashrao203@gmail.com>
2024-01-15 11:39:49 +0530

    server created with the node js and navigation

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require('fs');
+const { error } = require("console");
+const myServer = http.createServer((request, response) => {
+    // console.log('new req rec');
+    // console.log(request.headers)
+    const log = `${Date.now()}  ${request.url}: New Request Recieved\n`;
+    fs.appendFile('log.txt', log, (error, data) => {
+        switch (request.url) {
+            case "/":
+                response.end("homepage");
+                break;
+            case "/about":
+                response.end("Vikash");
+                break;
+            default:
+                response.end("404 Page not found");
+        }
+
+        // response.end('Hello from Server');
+    })
+    // console.log(request);
+});
+
+myServer.listen(8000, () => console.log('Server Started'));
