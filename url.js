99e5989cdb48335666fdb6e734dddd532a548629
vikash <vvikashrao203@gmail.com>
2024-01-15 18:51:17 +0530

    url file added

---
+++ url.js
@@ -0,0 +1,11 @@
+//URL 
+// uniform resource locator 
+
+// https://www.vikashh.tech/
+// protocol :
+// hypertext tranfer protocol secure
+// another is domain (www.vikashh.tech)
+
+// query parameters path 
+
+// vikash.dev/about?userID=1&a=2
