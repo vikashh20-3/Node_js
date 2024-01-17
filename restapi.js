1aa2a0d3f1f31179bc06695971ff5be661d19d2c
vikash <vvikashrao203@gmail.com>
2024-01-17 22:42:17 +0530

    fetching user details with id params

--- restapi.js
+++ restapi.js
@@ -12,6 +12,11 @@ const app = express();
 // GET /api/user/1 - Get the User with Id 1
 // GET /api/user/2 - Get the User with ID 2
 
+// dynamic path parameters 
+// GET /id/users/:id
+//colon means this is dynamic 
+// :id -> this is variable
+
 //POST /api/users =Create a new user
 // PATCH /api/user/id -Edit the user with Id 
 
@@ -30,6 +35,11 @@ app.get('/api/users', (request, response) => {
     response.json(users);
 })
 
+app.get('/api/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const user = users.find(user => user.id === id)
+    return response.json(user);
+})
 
 
 
