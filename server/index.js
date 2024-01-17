25addb5e59dcbb6429a37eee6323589fa663a0d9
vikash <vvikashrao203@gmail.com>
2024-01-17 09:30:11 +0530

    request method added

--- server/index.js
+++ server/index.js
@@ -4,7 +4,35 @@ const url = require("url");
 
 
 const { error } = require("console");
-const myServer = http.createServer((request, response) => {
+// const myServer = http.createServer((request, response) => {
+//     if (request.url === '/favicon.ico') return response.end();
+//     // console.log('new req rec');
+//     // console.log(request.headers);
+//     const log = `${Date.now()} ${request.method} ${request.url}: New Request Recieved\n`;
+//     const myUrl = url.parse(request.url, true);
+//     //here true make seperation of different query parameters
+//     console.log(myUrl);
+//     fs.appendFile('log.txt', log, (error, data) => {
+//         // switch (request.url) {
+//         // switch (request.url) {
+//         switch (myUrl.pathname) {
+//             case "/":
+//                 response.end("homepage");
+//                 break;
+//             case "/about":
+//                 const username = myUrl.query.myname
+//                 response.end(`hi ${username}`);
+//                 break;
+//             default:
+//                 response.end("404 Page not found");
+//         }
+
+//         // response.end('Hello from Server');
+//     })
+//     // console.log(request);
+// });
+
+function serverHandler(request, response) {
     if (request.url === '/favicon.ico') return response.end();
     // console.log('new req rec');
     // console.log(request.headers);
@@ -23,13 +51,19 @@ const myServer = http.createServer((request, response) => {
                 const username = myUrl.query.myname
                 response.end(`hi ${username}`);
                 break;
+
+              case "/signup":
+                if(request.method==='GET') response.end("this is signup form");
+                else if (request.method==='POST'){
+                    response.end("success");
+                }  
             default:
                 response.end("404 Page not found");
         }
 
         // response.end('Hello from Server');
     })
-    // console.log(request);
-});
+};
 
+const myServer = http.createServer((request, response) => { serverHandler (request,response)})
 myServer.listen(9000, () => console.log('Server Started'));
