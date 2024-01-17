a03bba95a6a55c8615ec24d689ee447fc0be3220
vikash <vvikashrao203@gmail.com>
2024-01-17 22:35:16 +0530

    rendering the user name with html

--- restapi.js
+++ restapi.js
@@ -1,23 +1,32 @@
 const express = require('express');
-const users =require('./MOCK_DATA.json');
+const users = require('./MOCK_DATA.json');
 
 
 const app = express();
 
+
 // we are designing rest api in json data 
-// GET /users - LIST all users 
-// GET /user/1 - Get the User with Id 1
-// GET /user/2 - Get the User with ID 2
 
-//POST /users =Create a new user
-// PATCH /user/id -Edit the user with Id 
+// GET /users - html document render  
+// GET /api/users - LIST all users JSON 
+// GET /api/user/1 - Get the User with Id 1
+// GET /api/user/2 - Get the User with ID 2
 
-// DELETE / users/1 -Detele the User with ID 1 
+//POST /api/users =Create a new user
+// PATCH /api/user/id -Edit the user with Id 
+
+// DELETE /api/ users/1 -Detele the User with ID 1 
 
 
 
 // ROUTES 
-app.get ('/users',(request,response)=>{
+
+app.get('/users', (request, response) => {
+    const html = `
+    <ul>${users.map((user) => `<li> ${user.first_name}</li>`).join("")}</ul>`
+    response.send(html);
+})
+app.get('/api/users', (request, response) => {
     response.json(users);
 })
 
