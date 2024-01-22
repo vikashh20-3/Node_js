d34d60a8725d33dedfc16fa0999f5878f8571fe0
vikash <vvikashrao203@gmail.com>
2024-01-22 09:35:55 +0530

    mongo db connected with localhost url

--- server/index.js
+++ server/index.js
@@ -8,8 +8,12 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+//connection
+mongoose.connect('mongodb://127.0.0.1:27017/demo-app-1')
+    .then(() => console.log("mongo db connected"))
+    .catch(err => console.log("mongo error", err))
 
-
+const User = mongoose.model("user", userSchema);
 // Schema of mongoose 
 const userSchema = new mongoose.Schema({
     fistname: {
