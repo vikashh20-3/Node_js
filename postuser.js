c19bd48e807cdb60949695893ccf6aeb0918b908
vikash <vvikashrao203@gmail.com>
2024-01-22 22:04:39 +0530

    Port added

--- postuser.js
+++ postuser.js
@@ -40,3 +40,44 @@ const userSchema = new mongoose.Schema({
 });
 const User = mongoose.model("user", userSchema);
 
+
+
+app.post('/api/users', async (request, response) => {
+    // TODO : create new user
+
+    const body = request.body;
+
+    // we will check that everything is available
+    if (
+        !body ||
+        !body.first_name ||
+        !body.last_name ||
+        !body.email ||
+        !body.gender ||
+        !body.job_title
+    ) {
+        return response.status(400).json({ msg: "All fields are required..." })
+    }
+
+    try {
+        const user = await User.create({
+            firstname: body.first_name,
+            lastname: body.last_name,
+            email: body.email,
+            gender: body.gender,
+            jobtitle: body.job_title,
+
+        });
+        return response.sendStatus(201).json({ msg: "succes to create a user", user });
+    }
+    catch (err) {
+        console.error(err);
+        return response.status(500).json({ msg: "Internal Server Error" });
+
+    }
+});
+
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log(`Server started at : ${PORT}`));
